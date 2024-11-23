3a9df0b39dc02db3f4a9cb39556cd474846c90d1
Petrechko Anastasiia <petrechko.pn@ucu.edu.ua>
2024-11-23 09:00:33 +0200

    add floyd-warshall algorithm

--- main/algorithms.py
+++ main/algorithms.py
@@ -181,3 +181,64 @@ def bellman_ford(graph, start, goal):
     path.reverse()
     end_time = time.time()
     return path, end_time - start_time
+
+
+def floyd_warshall(graph, start, end):
+    """
+    Implements the Floyd-Warshall algorithm for a graph in list format.
+
+    Args:
+        graph (dict): Graph as an list where graph[u] is a list of tuples (v, weight).
+        start (str): The starting node.
+        end (str): The ending node.
+
+    Returns:
+        tuple: (distance, path) where:
+               - distance (float): Shortest distance from start to end.
+               - path (list): List of nodes representing the shortest path, or None if no path exists.
+
+    Example:
+        >>> graph = {
+        ...     'a': [('b', 3), ('d', 7)],
+        ...     'b': [('c', 2)],
+        ...     'c': [('d', 1)],
+        ...     'd': [('a', 1)]
+        ... }
+        >>> floyd_warshall(graph, 'a', 'c')
+        (5, ['a', 'b', 'c'])
+        >>> floyd_warshall(graph, 'b', 'd')
+        (3, ['b', 'c', 'd'])
+        >>> floyd_warshall(graph, 'a', 'e')
+        (inf, None)
+    """
+    if start not in graph or end not in graph:
+        return float('inf'), None
+
+    nodes = graph.keys()
+    dist = {u: {v: float('inf') for v in nodes} for u in nodes}
+    next_node = {u: {v: None for v in nodes} for u in nodes}
+
+    for u in graph:
+        for v, weight in graph[u]:
+            dist[u][v] = weight
+            next_node[u][v] = v
+        dist[u][u] = 0
+
+    for k in nodes:
+        for i in nodes:
+            for j in nodes:
+                if dist[i][j] > dist[i][k] + dist[k][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
+                    next_node[i][j] = next_node[i][k]
+
+    def reconstruct_path(start, end):
+        if next_node[start][end] is None:
+            return None
+        path = [start]
+        while start != end:
+            start = next_node[start][end]
+            path.append(start)
+        return path
+
+    shortest_path = reconstruct_path(start, end)
+    return dist[start][end], shortest_path
