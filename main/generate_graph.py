97ce383fa811afbc9b85e6ef1d2c87acc64deaf9
Ivan Polyanskii <polianskyi.pn@ucu.edu.ua>
2024-12-02 02:22:26 +0200

    new version

--- main/generate_graph.py
+++ main/generate_graph.py
@@ -2,10 +2,8 @@ import random
 import string
 
 def generate_random_graph():
-    # Визначаємо кількість вершин від 10 до 100
     num_vertices = random.randint(10, 100)
     
-    # Функція для генерації унікальних ідентифікаторів вершин (a, b, ..., z, aa, ab, ...)
     def generate_vertex_names(n):
         names = []
         letters = string.ascii_lowercase
@@ -22,14 +20,11 @@ def generate_random_graph():
     
     vertices = generate_vertex_names(num_vertices)
     
-    # Визначаємо кількість ребер від 50 до 150, не більше можливих без повторів і без циклів
     max_edges = num_vertices * (num_vertices - 1) // 2
     num_edges = random.randint(50, min(150, max_edges))
     
-    # Генеруємо всі можливі ребра без повторів і без циклів
     possible_edges = [(u, v) for idx, u in enumerate(vertices) for v in vertices[idx+1:]]
     
-    # Випадково вибираємо необхідну кількість ребер
     selected_edges = random.sample(possible_edges, num_edges)
     
     graph = {vertex: [] for vertex in vertices}
@@ -39,4 +34,3 @@ def generate_random_graph():
         graph[v].append((u, weight))
     
     return graph
-print(generate_random_graph())
